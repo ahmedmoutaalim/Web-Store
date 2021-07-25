557c2565b16571cd78f22f427ef62b4a4f121598
Omar <57999049+ahmedmoutaalim@users.noreply.github.com>
2021-07-26 00:32:14 +0100

    ad create user option

--- Store/Store/Areas/Customer/Controllers/UserController.cs
+++ Store/Store/Areas/Customer/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Store.Data;
+using Store.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,16 +9,51 @@ using System.Threading.Tasks;
 
 namespace Store.Areas.Customer.Controllers
 {
+    [Area("customer")]
     public class UserController : Controller
     {
+        private UserManager<IdentityUser> _userManager;
+
+        private ApplicationDbContext _db;
+
+        public UserController(UserManager<IdentityUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
         public IActionResult Index()
+        {
+            var dd = _userManager.GetUserId(HttpContext.User);
+            return View(_db.ApplicationUsers.ToList());
+
+        }
+        public async Task<IActionResult>Create()
         {
             return View();
         }
 
-        public async Task<IActionResult> Create()
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
         {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (result.Succeeded)
+                {
+                  /*  var isSaveRole = await _userManager.AddToRoleAsync(user, "User");*/
+                    TempData["save"] = "User has been created successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+
             return View();
+
         }
     }
 }
